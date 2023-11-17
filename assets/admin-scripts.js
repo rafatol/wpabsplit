23ec5ff3a5876d6c1ad68c736115584528e89a4a
Rafael Toledo <rafamd@gmail.com>
2023-11-17 18:37:09 +0000

    Removendo métodos não utilizados pelo javascript

--- assets/admin-scripts.js
+++ assets/admin-scripts.js
@@ -92,53 +92,6 @@ var wpabsplit = {
             targetSelect.trigger('change');
         });
     },
-    changeTriggerType: function(){
-        let currentEl = jQuery(this);
-
-        if(jQuery('[data-parent="' + currentEl.attr('name') + '"]').length){
-            jQuery('[data-parent="' + currentEl.attr('name') + '"]').addClass('hidden');
-            jQuery('[data-parent="' + currentEl.attr('name') + '"] [name]').prop('required', false);
-
-            jQuery('[data-parent="' + currentEl.attr('name') + '"][data-value="' + currentEl.val() + '"]').removeClass('hidden');
-            jQuery('[data-parent="' + currentEl.attr('name') + '"][data-value="' + currentEl.val() + '"] [name]').prop('required', true);
-        }
-    },
-    addTrigger: function(e){
-        e.preventDefault();
-
-        let tbody = jQuery(this).closest('tbody');
-
-        jQuery('<tr>\n' +
-            '                <td><input type="text" name="trigger[@][selector]" id="trigger-@-selector" data-name="trigger[@][selector]" data-id="trigger-@-selector" data-invalid="%s is not a valid selector" required></td>\n' +
-            '                <td>\n' +
-            '                    <select name="trigger[@][type]" id="trigger-@-type" data-name="trigger[@][type]" data-id="trigger-@-type" required>\n' +
-            '                        <option value="click">Click</option>\n' +
-            '                        <option value="submit">Enviar</option>\n' +
-            '                        <option value="mousein">Mouse In</option>\n' +
-            '                        <option value="mouseout">Mouse Out</option>\n' +
-            '                        <option value="visible">Visível</option>\n' +
-            '                    </select>\n' +
-            '                </td>\n' +
-            '                <td><input type="text" name="trigger[@][description]" id="trigger-@-description" data-name="trigger[@][description]" data-id="trigger-@-description" required></td>\n' +
-            '                <td><button type="button" data-action="add">Adicionar</button> <button type="button" data-action="delete">Remover</button></td>\n' +
-            '            </tr>').appendTo(tbody);
-
-        wpabsplit.updateFieldName(tbody);
-    },
-    deleteTrigger: function(e){
-        e.preventDefault();
-
-        let currentRow = jQuery(this).closest('tr');
-        let tbody = currentRow.closest('tbody');
-
-        if(tbody.find('tr').length == 1){
-            return;
-        }
-
-        currentRow.remove();
-
-        wpabsplit.updateFieldName(tbody);
-    },
     updateFieldName: function(tbody){
         tbody.find('tr').each(function(idx, el){
             jQuery(el).find('[data-name]').each(function(idx2, el2){
