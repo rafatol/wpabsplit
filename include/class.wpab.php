581b95c595c454c0d1ce925c80cf628e21f3e75a
Rafael Toledo <rafamd@gmail.com>
2024-11-17 01:20:36 -0300

    Atualizando meta em todas as situações de atualização

--- include/class.wpab.php
+++ include/class.wpab.php
@@ -144,28 +144,29 @@ SQL;
         if(WPAB_POST_TYPE != get_post_type($post_id)){
             global $wpdb;
 
-            // verifica se o post salvo é um challenger ou hypothesis de um teste
+            // carrega todos os testes que possuem o post salvo como control ou hypothesis
             $query = <<<SQL
 SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key IN ('wpab_control_page', 'wpab_hypothesis_page') AND meta_value = {$post_id};
 SQL;
 
-            $testPostId = $wpdb->get_var($query);
+            $testPostIds = $wpdb->get_col($query);
 
-            if($testPostId){
-                $controlPage = get_post(WPAB_get_control($testPostId));
-                $hypothesisPage = get_post(WPAB_get_hypothesis($testPostId));
+            if($testPostIds){
+                foreach($testPostIds as $testPostId){
+                    $controlPage = get_post(WPAB_get_control($testPostId));
+                    $hypothesisPage = get_post(WPAB_get_hypothesis($testPostId));
 
-                if($controlPage){
-                    update_post_meta($testPostId, 'wpab_control_page_title', $controlPage->post_title);
-                    update_post_meta($testPostId, 'wpab_control_page_url', get_permalink($controlPage));
-                }
+                    if($controlPage){
+                        update_post_meta($testPostId, 'wpab_control_page_title', $controlPage->post_title);
+                        update_post_meta($testPostId, 'wpab_control_page_url', get_permalink($controlPage));
+                    }
 
-                if($hypothesisPage){
-                    update_post_meta($testPostId, 'wpab_hypothesis_page_title', $hypothesisPage->post_title);
-                    update_post_meta($testPostId, 'wpab_hypothesis_page_url', get_permalink($hypothesisPage));
+                    if($hypothesisPage){
+                        update_post_meta($testPostId, 'wpab_hypothesis_page_title', $hypothesisPage->post_title);
+                        update_post_meta($testPostId, 'wpab_hypothesis_page_url', get_permalink($hypothesisPage));
+                    }
                 }
             }
-
 
             return $post_id;
         }
